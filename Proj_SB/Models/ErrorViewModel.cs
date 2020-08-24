579b3c2bd3eb268deef078b221fd150e868f3539
Elior Dayari <307902254@STUD.colman.ac.il>
2020-08-24 14:45:48 +0300

    Add project files.

---
+++ Proj_SB/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Proj_SB.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
