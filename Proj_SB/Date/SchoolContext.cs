0f6e6b062babb2935dc194cbeee3fb1f7c33456e
Elior Dayari <307902254@STUD.colman.ac.il>
2020-08-25 11:59:10 +0300

    New update

---
+++ Proj_SB/Date/SchoolContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proj_SB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proj_SB.Date
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext (DbContextOptions<SchoolContext> options): base(options){
+            }
+
+        DbSet<Student> Student { get; set; }
+        DbSet<Course> Course { get; set; }
+        DbSet<Enrollment> Enrollment { get; set; }
+    }   
+}
+
+    
+
--- Proj_SB/Startup.cs
+++ Proj_SB/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Proj_SB.Date;
 
 namespace Proj_SB
 {
@@ -23,7 +25,10 @@ namespace Proj_SB
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddControllersWithViews();
+            services.AddDbContext<SchoolContext>(options =>
+           options.UseSqlServer(Configuration.GetConnectionString("SchoolContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
