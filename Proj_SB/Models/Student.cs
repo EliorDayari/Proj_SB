e34fd6fc1154db0af2adec482bed32cf29ad087a
Elior Dayari <307902254@STUD.colman.ac.il>
2020-08-25 11:31:00 +0300

    I've added some new Models(Enrollment,Course)

--- Proj_SB/Models/Student.cs
+++ Proj_SB/Models/Student.cs
@@ -5,17 +5,25 @@ using System.Threading.Tasks;
 
 namespace Proj_SB.Models
 {
+    public enum Grade
+    {
+        A,B,C,D,F
+    }
     public class Student
     {
         public int ID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public int Grade { get; set; }
+        public Grade? Grade { get; set; }
 
         public string Email { get; set; }
-        public string Color { get; set; }
+     
+        public DateTime EnrollmentDate { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
 
 
-        public int HahahHA { get; set; }
     }
+
+    
 }
